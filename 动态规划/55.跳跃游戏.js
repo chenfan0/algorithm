479a97c80ae1574fb4a2b2687095b09ba131973e
陈帆 <1490040662@qq.com>
2022-03-25 20:42:12 +0800

    跳跃游戏

--- 动态规划/55.跳跃游戏.js
+++ 动态规划/55.跳跃游戏.js
@@ -1,1 +1,25 @@
+// https://leetcode-cn.com/problems/jump-game/
+// 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+// 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+// 判断你是否能够到达最后一个下标。
 
+const nums = [2, 3, 1, 1, 4];
+
+function canJump(nums) {
+  // 如果只有nums的长度为1，直接返回true
+  if (nums.length === 1) return true
+  // 如nums的长度为0，直接返回false
+  if (nums[0] === 0) return false
+  // max用来记录当前所能到达的最远距离
+  let max = 0
+  for (let i = 0; i < nums.length - 1; i++) {
+    const item = nums[i]
+    // max 与 item + i 取大值
+    max = Math.max(max, item + i)
+    // 如果当前能到达的最大距离已经超过nums.length - 1，则直接返回true
+    if (max >= nums.length - 1) return true
+    // 如果当前跳跃步数为0，并且当前的位置是
+    if (item === 0 && i === max) return false
+  }
+
+}
