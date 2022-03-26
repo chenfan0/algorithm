b5f19a96dd6ca6b6a60d74886c88058539dcfc8f
陈帆 <1490040662@qq.com>
2022-03-26 22:28:36 +0800

    买卖股票的最佳时机2

---
+++ 动态规划/122.买卖股票的最佳时机2.js
@@ -0,0 +1,18 @@
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+// 给定一个数组 prices ，其中 prices[i] 表示股票第 i 天的价格。
+// 在每一天，你可能会决定购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以购买它，然后在 同一天 出售。
+// 返回 你能获得的 最大 利润 。
+
+function maxProfit(prices) {
+  const n = prices.length
+  // dp0表示当天手里没有股票的最大收益
+  // dp1表示当天手里有股票的最大收益
+  let dp0 = 0, dp1 = -prices[0]
+
+  for (let i = 1; i < n; i++) {
+    dp0 = Math.max(dp0, dp1 + prices[i])
+    dp1 = Math.max(dp1, dp0 - prices[i])
+  }
+  return dp0
+}
