ab9c1f7259e65a8dd9e847a7afe0e532228a2030
陈帆 <1490040662@qq.com>
2022-04-22 21:39:44 +0800

    螺旋矩阵

--- src/字节前端/54.螺旋矩阵.js
+++ src/字节前端/54.螺旋矩阵.js
@@ -6,36 +6,57 @@
  */
 
 function spiralOrder(matrix) {
-  const result = [];
-  const row = matrix.length;
-  const col = matrix[0].length;
-  let rowStart = 0,
-    rowEnd = row,
-    colStart = 0,
-    colEnd = col;
-  while (rowStart <= rowEnd && colStart <= colEnd) {
-    print(rowStart, rowEnd, colStart, colEnd)
-    rowStart++
-    colStart++
-    rowEnd--
-    colEnd--
+  const result = []
+  const row = matrix.length
+  const col = matrix[0].length
+  const length = row * col
+  let rowStart = 0, rowEnd = row, colStart = 0, colEnd = col
+
+  function isEnd(arr, length) {
+    return arr.length === length
   }
-  function print(i, rowEnd, j, colEnd) {
+
+  while (rowStart < rowEnd && colStart < colEnd) {
+    let i = rowStart, j = colStart
+    
+    // 向右遍历
     while (j < colEnd) {
       result.push(matrix[i][j++])
+      if (isEnd(result, length)) {
+        return result
+      }
     }
     j--
+    i++
+    // 向下遍历
     while (i < rowEnd) {
-      result.push(matrix[++i][j])
+      result.push(matrix[i++][j])
+      if (isEnd(result, length)) {
+        return result
+      }
     }
     i--
-    while (i !== rowEnd && j >= colStart) {
-      result.push(matrix[i][--j])
+    j--
+    // 向左遍历
+    while (j >= colStart) {
+      result.push(matrix[i][j--])
+      if (isEnd(result, length)) {
+        return result
+      }
     }
     j++
-    while (j !== colEnd && i > rowStart) {
-      result.push(matrix[--i][j])
+    i--
+    // 向上遍历
+    while (i > rowStart) {
+      result.push(matrix[i--][j])
+      if (isEnd(result, length)) {
+        return result
+      }
     }
+    rowStart++
+    rowEnd--
+    colStart++
+    colEnd--
   }
 
   return result
