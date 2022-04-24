829d13e7bab7d32e5d3dcc7a55731bc7722ffe08
陈帆 <1490040662@qq.com>
2022-04-24 21:23:36 +0800

    圆圈中最后剩下的数字

--- src/字节前端/62.圆圈中最后剩下的数字.js
+++ src/字节前端/62.圆圈中最后剩下的数字.js
@@ -5,9 +5,21 @@
  * @param {number} m
  * @return {number}
  */
-
+// 递归
 function lastRemaining(n, m) {
   if (n === 1) return 0
   const x = lastRemaining(n - 1, m)
   return (m + x) % n
+}
+
+// 迭代
+function lastRemaining(n, m) {
+  if (n === 1) return 0
+  let result = 0
+
+  for (let i = 2; i <= n; i++) {
+    result = (m + result) % i
+  }
+
+  return result
 }
