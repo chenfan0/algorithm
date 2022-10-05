5b050fe1d6fcc11e796540b33595b628842e27a9
chenfan.0 <chenfan.0@bytedance.com>
2022-10-05 16:29:46 +0800

    整数拆分

---
+++ src/动态规划/343.整数拆分.js
@@ -0,0 +1,19 @@
+// https://leetcode.cn/problems/integer-break/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var integerBreak = function(n) {
+  const dp = new Array(n + 1).fill(0)
+  dp[1] = 1
+  dp[2] = 1
+
+  for (let i = 3; i <= n; i++) {
+    // 这里 j <= (i >> 1)是因为 会出现重复现象， 比如 3 * 4  4 * 3
+    for (let j = 1; j <= (i >> 1); j++) {
+      dp[i] = Math.max(dp[i], dp[i - j] * j, (i - j) * j)
+    }
+  }
+  return dp[n]
+ }
