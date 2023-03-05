1b2ddcd36af6d86cf20f6225044b0d3205357d2a
chen fan <1490040662@qq.com>
2023-03-05 12:16:32 +0800

    feat: 复习跳跃游戏

--- src/动态规划/55.跳跃游戏.js
+++ src/动态规划/55.跳跃游戏.js
@@ -3,23 +3,22 @@
 // 数组中的每个元素代表你在该位置可以跳跃的最大长度。
 // 判断你是否能够到达最后一个下标。
 
-const nums = [2, 3, 1, 1, 4];
-
 function canJump(nums) {
-  // 如果只有nums的长度为1，直接返回true
-  if (nums.length === 1) return true
-  // 如nums的长度为0，直接返回false
-  if (nums[0] === 0) return false
-  // max用来记录当前所能到达的最远距离
-  let max = 0
-  for (let i = 0; i < nums.length - 1; i++) {
-    const item = nums[i]
-    // max 与 item + i 取大值
-    max = Math.max(max, item + i)
-    // 如果当前能到达的最大距离已经超过nums.length - 1，则直接返回true
-    if (max >= nums.length - 1) return true
-    // 如果当前跳跃步数为0，并且当前的位置是
-    if (item === 0 && i === max) return false
+  const len = nums.length
+  // 当前能到达的最远位置
+  let maxPos = 0
+
+  // 这里 len - 1, 是因为不需要到最后一个
+  for (let i = 0; i < len - 1; i++) {
+    const step = nums[i]
+    maxPos = Math.max(maxPos, i + step)
+    // 如果能到达的位置已经大于或等于数组长度，直接返回true
+    if (maxPos >= len) return true
+    // 如果当前到达最远位置了，并且step === 0说明走不到最远位置
+    if (maxPos === i && step === 0) {
+      return false
+    }
   }
+  return true
 
 }
