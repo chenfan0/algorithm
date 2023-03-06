475ba2fc1e31fc1c99a7427dd296826796801809
chen fan <1490040662@qq.com>
2023-03-06 21:32:31 +0800

    feat: 复习最大正方形

--- src/动态规划/221.最大正方形.js
+++ src/动态规划/221.最大正方形.js
@@ -3,27 +3,24 @@
 // 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
 
 function maximalSquare(matrix) {
-  const row = matrix.length,
-    col = matrix[0].length;
-  let max = 0;
-  const dp = new Array(row).fill(0).map(() => new Array(col).fill(0));
-
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < col; j++) {
-      if (i === 0 || j === 0) {
-        // 如果当前所处位置为最上方或者最左方
-        dp[i][j] = matrix[i][j] === "1" ? 1 : 0;
-        max = Math.max(dp[i][j], max);
-      } else if (i > 0 && j > 0) {
-        if (matrix[i][j] === "0") {
-          dp[i][j] = 0;
-        } else {
-          // dp[i][j]取左边右边，左上方的最小值 + 1
-          dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
-          max = Math.max(dp[i][j], max);
-        }
+  const m = matrix.length
+  const n  = matrix[0].length
+  // 以当前节点为矩形最右点
+  const dp = new Array(m).fill(0).map(_ => new Array(n).fill(0))
+  let res = 0
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === '0') {
+        continue
+      } else if (i === 0 || j === 0) {
+        dp[i][j] = 1
+        res = Math.max(1, res)
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+        res = Math.max(res, dp[i][j] ** 2)
       }
     }
   }
-  return max * max;
+
+  return res
 }
