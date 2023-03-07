3c676e3439f14a4d89b3d10008fab6c10eb100e7
chen fan <1490040662@qq.com>
2023-03-07 11:37:37 +0800

    feat: 复习打家劫舍III

--- src/动态规划/337.打家劫舍III.js
+++ src/动态规划/337.打家劫舍III.js
@@ -13,37 +13,19 @@
  * @return {number}
  */
  var rob = function(root) {
-  // map1用于记录 当 isRob为true时的节点
-  const map1 = new Map()
-  // map2用于记录 当 isRob为false时的节点
-  const map2 = new Map()
-  const dfs = (node, isRob) => {
-    if (isRob && map1.has(node)) return map1.get(node)
-    if (!isRob && map2.has(node)) return map2.get(node)
-    if (!node) return 0
-    if (isRob) {
-      // 当isRob为true，也就是当前节点是可以偷的
-      // 那么取 偷当前节点的最大值和不偷当前节点的最大值
-      const left = dfs(node.left, false)
-      map2.set(node.left, left) // false 用map2保存
-      const right = dfs(node.right, false)
-      map2.set(node.right, right) // false map2保存
-      const _left = dfs(node.left, true)
-      map1.set(node.left, _left) // true map1保存
-      const _right = dfs(node.right, true)
-      map1.set(node.right, _right) // true map1保存
-      const max = Math.max(left + right + node.val, _left + _right)
-      map1.set(node, max) // 当前的isRob为true，所以用map1保存
-      map2.set(node, _left + _right) // _left1 和 _left2都是true，则可以认为当前为isRob为false的取值
-      return max
-    } else {
-      const left = dfs(node.left, true)
-      map1.set(node.left, left)
-      const right = dfs(node.right, true)
-      map1.set(node.right, right)
-      map2.set(node, left + right)
-      return left + right
+
+  const dfs = (node) => {
+    if (!node) {
+      return [0, 0]  // 第一个 0 表示选择了当前节点对应的最大值，第二个 0 表示不选择当前节点对应的最大值
     }
+
+    const left = dfs(node.left)
+    const right = dfs(node.right)
+    const select = node.val + left[1] + right[1]
+    const noSelect = Math.max(left[0], left[1]) + Math.max(right[0], right[1])
+
+    return [select, noSelect]
   }
-  return Math.max(dfs(root, true), dfs(root, false))
+
+  return Math.max(...dfs(root))
 };
